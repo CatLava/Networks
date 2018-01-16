4725a17417b0fb90f6268c64babd8c465cb257c7
CatLava <evan.schoening@gmail.com>
2018-01-15 20:08:40 -0800

    Add files via upload
    NSlookup that provides ipv4 addresses for url

---
+++ nslook.py
@@ -0,0 +1,24 @@
+# python nslookup to add into shodan code
+import socket
+import re
+
+web =  input("enter url :")
+ips = socket.getaddrinfo(web, 80)
+iplst = []
+iplst2 = []
+lst = []
+master = []
+for i in ips:
+    iplst.append(i)
+    for j in iplst:
+        j = str(j)
+        if not j.startswith('(<AddressFamily.AF_INET:'): continue
+        iplst2.append(j)
+for items in iplst2:
+    m = items.split()
+    lst.append(m)
+    for i in lst:
+        k = i[5][2:-2]
+        master.append(k)
+master = set(master)
+print(master)
